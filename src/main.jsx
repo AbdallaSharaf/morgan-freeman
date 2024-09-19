c75890dd2c847f221d655fbcfcecf8b33b96978c
AbdallaSharaf <124005902+AbdallaSharaf@users.noreply.github.com>
2024-09-20 00:29:53 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import App from './pages/App.jsx'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Layout from './Layout/Layout.jsx'
 import PortfolioPage from './pages/PortfolioPage.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <BrowserRouter>
       <Routes>
       <Route path='/' element={<Layout />}>
@@ -17,5 +15,5 @@ createRoot(document.getElementById('root')).render(
       </Route>
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </>,
 )
